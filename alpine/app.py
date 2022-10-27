745ef98b4d2c2b182fb29d2bd225f69d0841a51e
Carlos Lizarraga <97635720+clizarraga-UAD7@users.noreply.github.com>
2022-10-27 09:23:08 -0700

    updated code examples

--- alpine/app.py
+++ alpine/app.py
@@ -1,2 +1,16 @@
-print('hello world!')
-print('this is my first attempt')
+# Python print example
+
+import datetime
+import calendar
+
+now = datetime.datetime.now()
+
+print("\n At this moment, the time is:", now)
+print("\n")
+
+# Print this year calendar 
+print(calendar.month(now.year, now.month))
+
+print('\n This output is from the app.py file copied into the alpine image')
+
+
